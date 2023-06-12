24f17b2f708a06adf17c2353ec9cf81eb07b4d11
Jubayer934 <mdnoshin62@gmail.com>
2023-06-12 09:05:17 +0600

    After mid

--- Graph Representation using Adjacency List.py
+++ Graph Representation using Adjacency List.py
@@ -5,4 +5,4 @@ graph= [[2,8],[2],[0,1,3],[2,8],[5,6,7],[4,6],[4,5],[4],[0,3]]
 
 for u in range(len(graph)):
 				for v in range(len(graph[u])):
-									print(u,'-->',v)
+									print(u,'-->',graph[u][v])
